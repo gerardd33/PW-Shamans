988b9a58d263210f06541cfd616e05845f02434f
gerardd33 <gerardd@interia.pl>
2020-02-03 00:01:31 +0100

    final submitted

---
+++ src/tests/CMakeFiles/bottomlessBagTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bottomlessBagTest.dir/bottomlessBagTest.cpp.o"
+  "bottomlessBagTest"
+  "bottomlessBagTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bottomlessBagTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
